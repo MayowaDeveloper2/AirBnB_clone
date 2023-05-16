5f8757592905642031c6e596c42087f8854f9355
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2023-05-16 06:35:31 -0700

    all tasks

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This script is the base model"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+
+    """Class from all other classes"""
+    def __init__(self):
+        """Initializes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Returns official string representation"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys"""
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isooformat()
+        return data
