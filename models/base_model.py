3e2aefe2b01e31dec20d9e1507f24369f056a8b4
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2023-05-16 06:53:36 -0700

    all task

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,12 @@ class BaseModel:
         """Initializes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns official string representation"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at"""
@@ -24,8 +25,8 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dictionary containing all keys"""
-        data = self.__dict__.copy()
-        data['__class__'] = self.__class__.__name__
-        data['created_at'] = self.created_at.isoformat()
-        data['updated_at'] = self.updated_at.isoformat()
-        return data
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
