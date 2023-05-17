9cb47416d319fada810911eafb852cc51f1db732
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2023-05-16 20:47:54 -0700

    all task

--- models/base_model.py
+++ models/base_model.py
@@ -9,13 +9,30 @@ class BaseModel:
 
     """Class from all other classes"""
     def __init__(self):
-        """Initializes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        """Initializes
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """Returns official string representation"""
+        """Returns official a string representation"""
         return "[{}] ({}) {}".\
             format(type(self).__name__, self.id, self.__dict__)
 
