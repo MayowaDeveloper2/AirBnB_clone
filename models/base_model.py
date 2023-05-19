42f417abd821bd145162f4779d5f447ef3f0832d
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2023-05-19 15:09:14 -0700

    all task

--- models/base_model.py
+++ models/base_model.py
@@ -5,15 +5,17 @@ import uuid
 from datetime import datetime
 from models import storage
 
+
 class BaseModel:
 
-    """Class from all other classes"""
+    """Class from which all other classes will inherit"""
+
     def __init__(self, *args, **kwargs):
-        """Initializes
+        """Initializes instance attributes
 
         Args:
             - *args: list of arguments
-            - **kwargs: dict of key values arguments
+            - **kwargs: dict of key-values arguments
         """
 
         if kwargs is not None and kwargs != {}:
@@ -33,17 +35,20 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """Returns official a string representation"""
+        """Returns official string representation"""
+
         return "[{}] ({}) {}".\
             format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at"""
+
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys"""
+        """returns a dictionary containing all keys/values of __dict__"""
+
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
